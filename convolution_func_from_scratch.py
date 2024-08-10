58df7fc7fb03b283c57d5700f174a5dd277850b3
PouriaGolsorkhi <152257136+PouriaGolsorkhi@users.noreply.github.com>
2024-08-10 17:44:55 +0330

    function improved/testcases added to the main file

--- convolution_func_from_scratch.py
+++ convolution_func_from_scratch.py
@@ -9,19 +9,19 @@ def manualConvolution_3x3(mat1, mat2):
 
 def applyConvOnImage(img, customKernel):
     
-    # if len(img.shape) ==3:
-    #     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
         
     img_height , img_width, dimensions = img.shape
     outputImg = np.zeros_like(img)
-    
-    for i in range(1, img_width-1):
-        for j in range(1, img_height-1):
+    paded_image = np.pad(img, ((1,1), (1,1), (0,0)), mode="constant")
+    for i in range(1, img_width):
+        for j in range(1, img_height):
             for k in range(dimensions):
-                tempMat = img[i-1:i+2 , j-1:j+2, k]
+                tempMat = paded_image[i-1:i+2 , j-1:j+2, k]
                 convRes = manualConvolution_3x3(tempMat, customKernel)
                 outputImg[i,j,k] = np.sum(convRes) 
             
+    outputImg = np.clip(outputImg, 0, 255).astype(np.uint8)
     
     return outputImg
             
