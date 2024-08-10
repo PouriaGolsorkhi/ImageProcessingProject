d3ed6e81c321f63759a3973869bd1f458f7ceba9
PouriaGolsorkhi <152257136+PouriaGolsorkhi@users.noreply.github.com>
2024-08-10 17:11:57 +0330

    manual convolution fully operational

--- convolution_func_from_scratch.py
+++ convolution_func_from_scratch.py
@@ -3,28 +3,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def manualConvolution_3x3(mat1, mat2):
-    outputmat = np.zeros_like(mat1)
-    height , width = mat1.shape
-    for i in range(0,width):
-        for j in range(0,height):
-            outputmat[i,j] = mat1[i,j]*mat2[i,j]
     
-    return outputmat
+    return np.sum(mat1*mat2);
             
 
 def applyConvOnImage(img, customKernel):
     
-    if len(img.shape) ==3:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # if len(img.shape) ==3:
+    #     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
-    img_height , img_width = img.shape
+    img_height , img_width, dimensions = img.shape
     outputImg = np.zeros_like(img)
     
     for i in range(1, img_width-1):
         for j in range(1, img_height-1):
-            tempMat = img[i-1:i+2 , j-1:j+2]
-            convRes = manualConvolution_3x3(tempMat, customKernel)
-            outputImg[i,j] = np.sum(convRes) 
+            for k in range(dimensions):
+                tempMat = img[i-1:i+2 , j-1:j+2, k]
+                convRes = manualConvolution_3x3(tempMat, customKernel)
+                outputImg[i,j,k] = np.sum(convRes) 
             
     
     return outputImg
